36fe2e26b6a12b4a0226dd80fbf323c56f61c3cf
Tanya von Degurechaff <72034257+Victorthemachine@users.noreply.github.com>
2020-12-16 19:39:28 +0100

    Update Config.java

--- PgClassesAndOtherSmallThings/src/pgclassesandothersmallthings/pgClasses/Config.java
+++ PgClassesAndOtherSmallThings/src/pgclassesandothersmallthings/pgClasses/Config.java
@@ -14,9 +14,9 @@ public class Config {
     public static final String API = "jdbc";
     public static final String CONN = "mysql";
     public static final String HOST = "localhost";
-    public static final String DATABASE = "dk-319_air";
-    public static final String USER = "dk-319";
-    public static final String PASSWORD = "tFsAcVVA";
+    public static final String DATABASE = "dk-3xx_air";
+    public static final String USER = "dk-3xx";
+    public static final String PASSWORD = "xxxxxx";
     public static final String URL = API + ":" + CONN + "://" + HOST + "/" + DATABASE;
     
 }
